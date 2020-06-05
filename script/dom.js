4b607f3e36d1dc9d859ffd91c165dd5f6ca3fafc
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2020-06-05 20:37:19 -0300

    adiciona tabela com funcionalidade de remoção

--- script/dom.js
+++ script/dom.js
@@ -1,3 +1,8 @@
+$(document).ready(() => {
+  console.log("ready");
+  renderTable(taskList.tasks);
+});
+
 $("#form-new").on("submit", (e) => {
   e.preventDefault();
   console.log("form submit");
@@ -15,3 +20,70 @@ $("#form-new").on("submit", (e) => {
 
   taskList.addFromJson(taskObj);
 });
+
+$(document).on("click", "button[data-function]", (e) => {
+  let chosen = $(e.target).attr("data-function");
+  let rowId = $(e.target).closest("tr").children().first().text();
+
+  switch (chosen) {
+    case "complete":
+      break;
+    case "edit":
+      break;
+    case "delete":
+      taskList.removeByOrder(rowId);
+      break;
+  }
+});
+
+const categoryTranslate = {
+  work: "Trabalho",
+  home: "Casa",
+  university: "Universidade",
+  personal: "Pessoal",
+  shop: "Compras",
+};
+
+const renderTable = (tasks) => {
+  // adicionar tradução da categoria
+  // adicionar ações
+  // adicionar color code pra prioridade
+  let tbody = $("#list-body");
+  tbody.empty();
+  if (tasks && tasks.length > 0) {
+    tasks.forEach((task) => {
+      tbody.append(`
+        <tr class="${task.completed ? "table-secondary" : ""}">
+          <td>${task.order}</td>
+          <td>${task.name}</td>
+          <td>${task.priority}</td>
+          <td>${categoryTranslate[task.category] || "-"}</td>
+          <td class="py-2">
+            ${
+              task.completed
+                ? `<button type="button" data-function="delete" class="btn btn-light btn-sm" data-toggle="tooltip" data-placement="top" title="Remover">🗑</button>`
+                : `
+                  <button type="button" data-function="complete" class="btn btn-light btn-sm" data-toggle="tooltip" data-placement="top" title="Completar">✔</button>
+                  <button type="button" data-function="edit" class="btn btn-light btn-sm" data-toggle="tooltip" data-placement="top" title="Editar">✏</button>
+                  <button type="button" data-function="delete" class="btn btn-light btn-sm" data-toggle="tooltip" data-placement="top" title="Remover">🗑</button>
+                `
+            }
+
+            
+          </td>
+        </tr>
+      `);
+    });
+  } else {
+    tbody.append(`
+      <tr class="table-secondary">
+        <td colspan="5" class="text-center">
+          <span>Nenhuma tarefa cadastrada</span>
+        </td>
+      </tr>
+    `);
+  }
+
+  $(".tooltip").remove();
+  $('[data-toggle="tooltip"]').tooltip();
+};
--- script/store.js
+++ script/store.js
@@ -2,10 +2,11 @@ class TaskList {
   /** @type Task[] @description List of tasks */
   tasks;
   /** @type string @description Last prop used for sorting */
-  lastSort = "order";
+  lastSort;
 
   constructor(tasks) {
     this.tasks = tasks;
+    this.lastSort = "order";
   }
 
   incrementOrder = () => {
@@ -17,6 +18,7 @@ class TaskList {
   sort = (prop) => {
     this.lastSort = prop;
     // TODO
+    renderTable(this.tasks);
   };
 
   /**
@@ -28,6 +30,18 @@ class TaskList {
     this.sort();
     console.log(this.tasks);
   };
+
+  removeByOrder = (order) => {
+    let index = this.tasks.findIndex((task) => task.order == order);
+    console.log("Remove index:", index);
+    this.tasks.splice(index, 1);
+    if (this.tasks.length > 0) {
+      for (i = index; i < this.tasks.length; i++) {
+        this.tasks[i].order--;
+      }
+    }
+    renderTable(this.tasks);
+  };
 }
 
 class Task {
@@ -35,16 +49,18 @@ class Task {
   name;
   category;
   priority; // 1 a 5
+  completed;
 
   constructor({ order, name, category, priority }) {
     this.order = order;
     this.name = name;
     this.category = category;
     this.priority = priority;
+    this.completed = false;
   }
 }
 
-const taskList = new List([
+const taskList = new TaskList([
   new Task({
     order: 1,
     name: "Nome da tarefa",
